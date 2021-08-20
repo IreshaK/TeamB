8e2b256e1f3d0fa4f404d7493bc46adcd7c923b2
Smita <smita@smitas-air.lan>
2021-08-20 10:32:19 +1000

    Updated with all 5 mathematical functions

--- CalculatorFunctions.js
+++ CalculatorFunctions.js
@@ -1,3 +1,27 @@
+// Addition
+function Addition(a,b)
+{
+var add=a+b
+return add
+}
+console.log(Addition(10,5))
+
+// Subtraction
+function Subtraction(a,b)
+{
+var diff=a-b
+return diff
+}
+console.log(Subtraction(10,5))
+
+// Multiplication
+function Multiplication(a,b)
+{
+var product=a*b
+return product
+}
+console.log(Multiplication(10,5))
+
 // Division
 function Division(a,b)
 {
@@ -5,4 +29,13 @@ var div=a/b
 return div
 }
 console.log(Division(10,5))
+
+// Power
+function Power(a,b)
+{
+var pow=a**b
+return pow
+}
+console.log(Power(10,5))
+
 
